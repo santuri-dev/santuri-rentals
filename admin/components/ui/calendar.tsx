b7dfdeb78145d0cf3dabaf051250b77e485879ad
dannywamuya <dwamuya@gmail.com>
2024-09-18 11:34:18 +0300

    fix calendar component build error admin

--- admin/components/ui/calendar.tsx
+++ admin/components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as React from 'react';
-import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
@@ -59,10 +58,7 @@ function Calendar({
 				day_hidden: 'invisible',
 				...classNames,
 			}}
-			components={{
-				IconLeft: () => <ChevronLeftIcon className='h-4 w-4' />,
-				IconRight: () => <ChevronRightIcon className='h-4 w-4' />,
-			}}
+			components={{}}
 			{...props}
 		/>
 	);
