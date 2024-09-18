486b586e46e13b6043905d471b11689a053203f8
dannywamuya <dwamuya@gmail.com>
2024-09-18 11:36:57 +0300

    reinstall calendar component

--- admin/components/ui/calendar.tsx
+++ admin/components/ui/calendar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
@@ -58,7 +59,10 @@ function Calendar({
 				day_hidden: 'invisible',
 				...classNames,
 			}}
-			components={{}}
+			components={{
+				IconLeft: ({}) => <ChevronLeftIcon className='h-4 w-4' />,
+				IconRight: ({}) => <ChevronRightIcon className='h-4 w-4' />,
+			}}
 			{...props}
 		/>
 	);
