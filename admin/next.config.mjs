717bab7eee13a588d7a031a51ff6de4337d67643
dannywamuya <dwamuya@gmail.com>
2024-11-28 10:46:01 +0300

    fix rls

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: 'lmmyifayydtcperivjkc.supabase.co',
+				hostname: 'viahktdztuuesnnckrqo.supabase.co',
 			},
 		],
 		minimumCacheTTL: 60,
