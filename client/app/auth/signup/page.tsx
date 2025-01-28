8fc762b5b40a7b45846c4913ff74cf0b6c0dfd66
dannywamuya <dwamuya@gmail.com>
2025-01-28 10:01:44 +0300

    fix client build errors

--- client/app/auth/signup/page.tsx
+++ client/app/auth/signup/page.tsx
@@ -1,15 +1,23 @@
 'use client';
 
+import { Suspense } from 'react';
 import SignupForm from '@/components/Forms/auth/SignupForm';
 import { useSearchParams } from 'next/navigation';
+import { Skeleton } from '@/components/ui/skeleton';
 
-export default function Page() {
+function SignupContent() {
 	const search = useSearchParams();
 	const token = search.get('token');
 
+	return <SignupForm token={token} />;
+}
+
+export default function Page() {
 	return (
 		<div className='flex h-screen flex-col items-center justify-center sm:px-6 lg:px-8'>
-			<SignupForm token={token} />
+			<Suspense fallback={<Skeleton className='w-full h-full' />}>
+				<SignupContent />
+			</Suspense>
 		</div>
 	);
 }
